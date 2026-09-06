1ddc9c2a05a97fffbabbed7e3394fbb4190755dc
ll5199 <33507365+ll5199@users.noreply.github.com>
2026-09-06 04:52:13 +0000

    release: prepare v1.4.4

--- config.go
+++ config.go
@@ -17,7 +17,7 @@ import (
 const (
 	defaultBaseURL = "https://api.velyn65.com/v1"
 	providerID     = "yunqiao_bridge"
-	appVersion     = "1.4.3"
+	appVersion     = "1.4.4"
 )
 
 type appConfig struct {
